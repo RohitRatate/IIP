4a00e3cf4390ac707055b4b39ffe80d14acca9f8
unknown <rohitratate855@gmail.com>
2026-09-21 17:43:01 +0530

    TATA logo

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Ensures assets are loaded relative to the deployment path
   server: {
     port: 3000,
     open: true
